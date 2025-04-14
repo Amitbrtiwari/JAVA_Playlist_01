83eade15779f04225ebaa0f5f431d918efc90bd5
Amit Tiwari <amitbrtiwari@gmail.com>
2025-04-14 13:12:56 +0100

    Add files via upload

---
+++ PerimeterSquare.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class PerimeterSquare {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter side length of the square: ");
+        double side = sc.nextDouble();
+        double perimeter = 4 * side;
+        System.out.println("Perimeter of Square: " + perimeter);
+    }
+}
